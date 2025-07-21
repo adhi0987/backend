c267402ecd52afdf77032ad0aac9a715866ea62b
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-07-21 22:55:15 +0530

    add landing

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+def landing_page(request):
+    return render("landing_page.html")
 def home(request):
     return HttpResponse("This is the home page")
 
